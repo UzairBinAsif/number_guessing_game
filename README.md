89cf2f031a59ccbc3292ce44fd0266f053ad3080
UzairBinAsif <160782214+UzairBinAsif@users.noreply.github.com>
2024-03-23 01:06:51 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# number_guessing_game
+ This is my second project of Governor Initiative using Typescript, node.js and Inquirer.
---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("\nA game by Uzair Bin Asif\n")
+
+const randomNumber = Math.floor(Math.random() * 10 + 1)
+
+const answer = await inquirer.prompt([{
+    name: "guessedNumber",
+    type: "number",
+    message: "Please guess a number between 1 and 10:",
+}])
+
+if(answer.guessedNumber === randomNumber) {
+    console.log("Congratulations, you guessed right number")
+} else {
+    console.log("You guessed a wrong number");
+}
